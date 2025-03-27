4c98c5da5b75d0c0d069ade9b8f204d3b6cce060
Blancy <ievarituma9877@gmail.com>
2025-03-27 19:32:39 +0100

    Angular + Boostrap setted

---
+++ ViatgesCascinoAngular/src/app/components/hover/hover.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hover',
+  imports: [],
+  templateUrl: './hover.component.html',
+  styleUrl: './hover.component.css'
+})
+export class HoverComponent {
+
+}
